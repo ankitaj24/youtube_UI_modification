ae7dd1bd856e531abe9bff972a93eae9e097d0db
ankitaj24 <86417055+ankitaj24@users.noreply.github.com>
2021-09-25 17:16:58 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,8 @@
+(function (){
+    function fnAddButtons() {
+        var img=document.createElement("img");
+        img.src="https://img.icons8.com/ios-filled/500/000000/javascript.png";
+        document.querySelector("#donation-shelf.style-scope.ytd-watch-flexy").appendChild(img);        
+    }
+    fnAddButtons();
+})();
